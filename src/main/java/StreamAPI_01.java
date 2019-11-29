ea8e39189b13b2cc2bbe2e825dc93d962e07ddef
sombrero104 <sombrero.khy@gmail.com>
2019-11-29 15:34:57 +0900

    .

--- src/main/java/StreamAPI_01.java
+++ src/main/java/StreamAPI_01.java
@@ -10,9 +10,19 @@ public class StreamAPI_01 {
 
         /**
          * 1. forEach
+         * stream 요소를 순회.
          */
         Arrays.asList(1,2,3).stream()
-                .forEach(System.out::println);
+                .forEach(System.out::println); // 1,2,3
+
+        /**
+         * 2. map
+         * stream 개별 요소마다 연산 수행.
+         * 아래 코드는 리스트에 있는 요소의 제곱 연산을 수행한다.
+         */
+        Arrays.asList(1,2,3).stream()
+                .map(i -> i*i)
+                .forEach(System.out::println); // 1,4,9
     }
 
 }
