a8d012574cfec2c7d5d7ec675039351b70fc2c0c
sombrero104 <sombrero.khy@gmail.com>
2019-11-29 15:59:58 +0900

    .

--- src/main/java/StreamAPI_01.java
+++ src/main/java/StreamAPI_01.java
@@ -55,6 +55,33 @@ public class StreamAPI_01 {
         Arrays.asList(1,2,3).stream()
                 .filter(i -> i<=2)
                 .forEach(System.out::println); // 1,2
+
+        /**
+         * 6. flatMap
+         * stream의 내부에 있는 객체들을 연결한 stream을 반환한다.
+         */
+        System.out.println("[flatMap]-----------------------------------");
+        Arrays.asList(Arrays.asList(1,2), Arrays.asList(3,4,5), Arrays.asList(6,7,8,9)).stream()
+                .flatMap(i -> i.stream())
+                .forEach(System.out::println); // 1,2,3,4,5,6,7,8,9
+
+        /**
+         * 7. reduce
+         * stream을 단일 요소로 반환한다.
+         * 아래 코드에서는 첫번째 연산으로 1과 2가 선택되고,
+         * 계산식은 앞의 값에서 뒤의 값을 빼는 것이기 때문에 결과가 -1이 된다.
+         * 그리고 바로 다음 값 3이 선택되고 계산식이 -1-3이 되어서 최종 결과로는 -4가 된다.
+         */
+        System.out.println("[reduce]-----------------------------------");
+        int reduce = Arrays.asList(1,2,3).stream()
+                .reduce((a,b) -> a-b)
+                .get();
+        System.out.println(reduce); // -4
+
+        /**
+         * 8. collection
+         * 아래의 코드들은 각각의 메소드로 컬렉션 객체를 만들어서 반환한다.
+         */
     }
 
 }
